db8357a303e3b181cc7cc5ef1465ea504f3fb475
anthony <anthony.levasseur@reseau.eseo.fr>
2018-01-22 14:38:30 +0100

    ajout joseph

--- ManInTheMiddle.js
+++ ManInTheMiddle.js
@@ -1,11 +1,11 @@
 
-function valider(idInputDepart, idBoutonDepart, idConteneurDestination) {
+function valider(idInputDepart, idBoutonDepart, idConteneurDestination, reponse) {
     var inputDepart = document.getElementById(idInputDepart);
     var boutonDepart = document.getElementById(idBoutonDepart);
     var elementSuivant = document.getElementById(idConteneurDestination);
 
     boutonDepart.addEventListener("click",function () {
-        if (inputDepart.valueOf().value=="test"){
+        if (inputDepart.valueOf().value==reponse){
             console.log("inputDepart = "+inputDepart.valueOf().value);
             elementSuivant.setAttribute("style","");
         }
@@ -20,17 +20,55 @@ function aide(idBouton, idInput) {
     var contenuAide = document.getElementById(idInput);
 
     boutonAide.addEventListener("click", function () {
+        console.log(contenuAide.getAttribute("style"));
         contenuAide.setAttribute("style", "");
     })
 
 }
 
 window.onload=function() {
-    valider("consoleConnexionFtp", "BoutonValidationConnexionFtp", "ConnexionUtilisateur");
+    valider("consoleConnexionFtp", "BoutonValidationConnexionFtp", "ConnexionUtilisateur", "test");
     aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
-    valider("consoleConnexionUtilisateur", "BoutonValidationConnexionUtilisateur", "ConnexionMdp");
-    valider("consoleConnexionMdp", "BoutonValidationConnexionMdp", "ContenuServeur");
-    valider("consoleContenuServeur", "BoutonValidationContenuServeur", "Deconnexion");
-    valider("consoleDeconnexion", "BoutonValidationDeconnexion", "DeplacementDossier");
-    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier");
+
+    valider("consoleConnexionUtilisateur", "BoutonValidationConnexionUtilisateur", "ConnexionMdp", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleConnexionMdp", "BoutonValidationConnexionMdp", "ContenuServeur", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleContenuServeur", "BoutonValidationContenuServeur", "Deconnexion", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeconnexion", "BoutonValidationDeconnexion", "DeplacementDossier", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleCreationFichier", "BoutonValidationCreationFichier", "VisualisationCommandeEcritureFichier", "test");
+    //aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+//////////////////////
+
+    var boutonValidation = document.getElementById("BoutonValidationVisualisationCommandeEcritureFichier");
+    var container = document.getElementById("ConnexionAdminMachineCapture");
+    boutonValidation.addEventListener("click", function () {
+        container.setAttribute("style", "");
+    });
+
+    /*
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+
+    valider("consoleDeplacementDossier", "BoutonValidationDeplacementDossier", "CreationFichier", "test");
+    aide("boutonSolutionConnexionFtp","solutionConnexionFtp");
+    */
 }
